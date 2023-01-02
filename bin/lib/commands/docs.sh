1644c43e63942510530dc826fa561da1f90e90ad
Luis Carlos Aveiro <git@luisaveiro.com>
2023-01-02 07:06:28 +0000

    refactor: clean up explain commands code.

--- bin/lib/commands/docs.sh
+++ bin/lib/commands/docs.sh
@@ -30,3 +30,32 @@ function command::docs() {
 
   browser::open "${readme_url}"
 }
+
+#######################################
+# Display helpful information for
+# the docs command.
+#
+# Globals:
+#   APP_COMMAND
+#   APP_NAME
+#   GIT_REPOSITORY
+#
+# Outputs:
+#   Writes helpful information to
+#   stdout.
+#######################################
+function explain::docs() {
+  local helpful_tips=(
+    "To open the docs URL in your default browser:"
+    "${APP_COMMAND} docs"
+  )
+
+  explain::display_description \
+    "Open ${APP_NAME} documentation in your default browser."
+
+  explain::display_usage "docs"
+
+  explain::display_helpful_tips "${helpful_tips[@]}"
+
+  explain::display_more_information "${GIT_REPOSITORY}#docs-command"
+}
